a93bf749e6b978e0ec4cff3b047a05abe25aeaa2
chonlapat323 <cschonlapat@gmail.com>
2025-07-05 22:25:45 +0700

    chore: configure project settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['localhost'], // เพิ่ม domains ที่อนุญาตให้โหลดรูปได้
+  },
 };
 
 export default nextConfig;
